124e21f98157bd14eafc87ef06ff599d4358f242
WangYang-wy <1070942929@qq.com>
2018-04-11 15:13:33 +0800

    Accept: 176. Second Highest Salary in MySQL.

---
+++ MySQL/176. Second Highest Salary.sql
@@ -0,0 +1,9 @@
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+
+-- select * from Employee order by Salary;
+
+select MAX(Salary) as SecondHighestSalary from Employee where Salary < (select MAX(Salary) from Employee);
