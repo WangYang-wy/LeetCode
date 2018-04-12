4d74d65ab327b248784ce4ef0d8f41eaabddb4db
WangYang-wy <1070942929@qq.com>
2018-04-12 22:47:07 +0800

    Accept: MySQL/627. Swap Salary.sql in MySQL.

---
+++ MySQL/627. Swap Salary.sql
@@ -0,0 +1,13 @@
+create table if not exists salary (
+  id     int,
+  name   varchar(100),
+  sex    char(1),
+  salary int
+);
+Truncate table salary;
+insert into salary (id, name, sex, salary) values (1, 'A', 'm', 2500);
+insert into salary (id, name, sex, salary) values (2, 'B', 'f', 1500);
+insert into salary (id, name, sex, salary) values (3, 'C', 'm', 5500);
+insert into salary (id, name, sex, salary) values (4, 'D', 'f', 500);
+
+update salary set sex=IF(sex='m','f', 'm')
