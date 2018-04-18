767c7f491fe3b3e4fe2fc42860beb16d5b9113ca
WangYang-wy <1070942929@qq.com>
2018-04-18 10:27:08 +0800

    Accept:  476. Number Complement in C++.

--- C++/476. Number Complement.cpp
+++ C++/476. Number Complement.cpp
@@ -15,28 +15,10 @@ public:
         }
         return res;
     }
-
-    void printByBit(int num) {
-        std::cout << "从最高位到最低位打印一个32位整数的各个bit位：" << std::endl;
-        for (int i = 0; i < 32; i++) {
-            std::cout << "第" << i + 1 << "位的值为：" << ((num & (1 << (31 - i))) >> (31 - i)) << "." << std::endl;
-        }
-    }
-
-    void printByBit_(int num) {
-        int n = ceil(log2(num));
-        std::cout << "从最高位到最低位打印一个32位整数的各个bit位：" << std::endl;
-        for (int i = 0; i < n; i++) {
-            int bit = ((num & (1 << (n - i - 1))) >> (n - i - 1));
-            std::cout << "第" << i + 1 << "位的值为：" << bit << "." << std::endl;
-        }
-    }
 };
 
 int main() {
     Solution *solution = new Solution();
     std::cout << solution->findComplement(5) << std::endl;
-//    solution->printByBit_(5);
-//    std::cout << ((1 << 31)) << std::endl;
     return 0;
 }
