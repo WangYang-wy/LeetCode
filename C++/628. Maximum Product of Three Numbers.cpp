1cbc3aed5fd76147dbaf4202d6919d84d88662f1
WangYang-wy <1070942929@qq.com>
2018-05-03 22:47:04 +0800

    Update Faster: 628. Maximum Product of Three Numbers in C++.

--- C++/628. Maximum Product of Three Numbers.cpp
+++ C++/628. Maximum Product of Three Numbers.cpp
@@ -6,7 +6,32 @@
 
 class Solution {
 public:
+    // O(n).
     int maximumProduct(vector<int> &nums) {
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
+
+        for (auto &i: nums) {
+            if (i > max1) {
+                max3 = max2, max2 = max1, max1 = i;
+            } else if (i > max2) {
+                max3 = max2, max2 = i;
+            } else if (i > max3) {
+                max3 = i;
+            }
+
+            if (i < min1) {
+                min2 = min1, min1 = i;
+            } else if (i < min2) {
+                min2 = i;
+            }
+        }
+
+        return max(max1 * max2 * max3, max1 * min1 * min2);
+    }
+
+    // O(nlogn).
+    int maximumProduct_lower(vector<int> &nums) {
         int n = int(nums.size());
         sort(nums.begin(), nums.end());
         int index = -1;
@@ -32,7 +57,7 @@ public:
         } else if (detla <= 0) { // 全是负数。
             return last_3;
         } else if (1 == detla) { // 只有一个正数。
-            return nums[n - 1] * nums[0] * nums[1];
+            return nums[n - 1] * first_2;
         } else {
             return last_3 > first_2 * nums[n - 1] ? last_3 : first_2 * nums[n - 1];
         }
