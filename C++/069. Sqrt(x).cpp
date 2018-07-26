317e8253f65e9f8e4eac4c30e254ce6694745249
WangYang-wy <1070942929@qq.com>
2018-07-26 18:59:20 +0800

    Update: 069. Sqrt(x) in C++.

--- C++/069. Sqrt(x).cpp
+++ C++/069. Sqrt(x).cpp
@@ -14,6 +14,27 @@ public:
     int mySqrt(int x) {
         return int(sqrt(x));
     }
+
+    // 第二种解法：二分的方式。
+    bool guess(int x, int y) {
+        return x * x <= y;
+    }
+
+    int mySqrt(int x) {
+        int left = 0;
+        int right = x; // 二分的区间，左闭右开。
+        int ans = 0;
+        while (left < right) {
+            int mid = left + (right - left) / 2; // 防止溢出。
+            if (this->guess(mid, x)) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
 };
 
 int main() {
